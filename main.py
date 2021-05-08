85cc5c993634f116798dfbab3753f420c5833e13
regulad <regulad@outlook.com>
2021-05-08 15:27:23 -0400

    Add discrim

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ activeExtensionManager = extensionManager(bot)
 @bot.event
 async def on_ready():
   tprint('PepperCord', font = 'xsans')
-  print(f'Logged in as {bot.user.name} ({bot.user.id})')
+  print(f'Logged in as {bot.user.name}#{bot.user.discriminator} ({bot.user.id})')
 
 if __name__ == '__main__':
   print(activeExtensionManager.loadExtension())

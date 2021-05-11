8bf6358e33e95449a08dc1f0076f0db8452e3141
regulad <regulad@outlook.com>
2021-05-10 21:23:56 -0400

    Neat development tool

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from art import tprint
 
 activeExtensionManager = ExtensionManager(bot)
 
-
 @bot.event
 async def on_ready():
   tprint('PepperCord', font = 'xsans')
   print(f'Logged in as {bot.user.name}#{bot.user.discriminator} ({bot.user.id})')
 
 if __name__ == '__main__':
-  extensionLoader = copy.deepcopy(activeExtensionManager.loadExtension())
+  bot.load_extension('jishaku')
+  extensionLoader = activeExtensionManager.loadExtension()
   if extensionLoader and (len(extensionLoader) > 0):
     print(f'{extensionLoader} failed to load.')
   print(activeExtensionManager.listExtensions())

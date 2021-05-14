e64994d52893e7ff785e81e254e9ae98550b40d0
regulad <regulad@outlook.com>
2021-05-14 17:51:53 -0400

    APIs

--- cogs/apis.py
+++ cogs/apis.py
@@ -149,6 +149,30 @@ class apis(
         embed = discord.Embed(title=name).set_image(url=f"https://crafatar.com/renders/body/{uuid}?overlay")
         await ctx.send(embed=embed)
 
+    @commands.command(
+        name="bored",
+        description="Do something, stop being bored!",
+        brief="Anti-boredom."
+    )
+    async def bored(self, ctx):
+        async with aiohttp.ClientSession() as client:
+            async with client.get("https://www.boredapi.com/api/activity/") as request:
+                result = await request.json()
+        embed = discord.Embed(title=result["type"], description=result["activity"])
+        await ctx.send(embed=embed)
+
+    @commands.command(
+        name="quote",
+        description="Get inspired from a random quote.",
+        brief="Get inspired!"
+    )
+    async def quote(self, ctx):
+        async with aiohttp.ClientSession() as client:
+            async with client.get("https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en") as request:
+                result = await request.json()
+        embed = discord.Embed(title=result["quoteText"]).set_author(name=result["quoteAuthor"])
+        await ctx.send(embed=embed)
+
 
 def setup(bot):
     bot.add_cog(apis(bot))

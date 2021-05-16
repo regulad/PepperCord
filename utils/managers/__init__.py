8bb6f422ace5b76f0528e841bbf0df8d555beaa1
regulad <52430642+regulad@users.noreply.github.com>
2021-05-15 21:12:31 -0400

    Random bot shiz :tm:

---
+++ utils/managers/__init__.py
@@ -0,0 +1,3 @@
+from .common import *
+from .guild import *
+from .user import *
---
+++ utils/managers/common.py
@@ -0,0 +1,51 @@
+import typing
+
+import discord
+import pymongo
+
+
+class CommonConfigManager:
+    def __init__(
+        self,
+        model: typing.Union[discord.Guild, discord.Member, discord.User],
+        collection: pymongo.collection.Collection,
+        key_name: str,
+        key_value,
+    ):
+        self.model = model
+        self.collection = collection
+
+        active_dict = collection.find_one({"_id": model.id})
+        if not active_dict:
+            self.active_dict = {"_id": model.id}
+            collection.insert_one(self.active_dict)
+        else:
+            self.active_dict = collection.find_one({"_id": model.id})
+
+        self.key_name = key_name
+        self.key_value = key_value
+
+        self.active_key = self.active_dict.setdefault(key_name, key_value)
+
+    def read(self):
+        return self.active_key
+
+    def write(self, key_value):
+        working_dict = {str(self.key_name): key_value}
+        write_query = {"$set": working_dict}
+        self.collection.update_one({"_id": self.active_dict["_id"]}, write_query)
+
+    def delete(self, key_value):
+        working_dict = {str(self.key_name): key_value}
+        write_query = {"$unset": working_dict}
+        self.collection.update_one({"_id": self.active_dict["_id"]}, write_query)
+
+
+class BlacklistManager(CommonConfigManager):
+    def __init__(
+        self, model: typing.Union[discord.Guild, discord.Member, discord.User], collection: pymongo.collection.Collection
+    ):
+        super().__init__(model, collection, "blacklisted", False)
+
+    def write(self, value: bool):
+        super().write(value)

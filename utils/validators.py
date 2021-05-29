082fd005e8e07d22000cbf54c6399af9a8589d48
regulad <52430642+regulad@users.noreply.github.com>
2021-05-29 11:42:44 -0400

    queue/saved playlists

---
+++ utils/validators.py
@@ -0,0 +1,9 @@
+from urllib.parse import urlparse
+
+
+def str_is_url(url: str):
+    try:
+        parsed = urlparse(url)
+        return all([parsed.scheme, parsed.netloc])
+    except ValueError:
+        return False

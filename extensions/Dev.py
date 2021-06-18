897e2cce581a2539ed3842b73edd722cf6c16763
regulad <52430642+regulad@users.noreply.github.com>
2021-06-18 15:21:50 -0400

    Dev.py typehinting & formatting changes

--- extensions/Dev.py
+++ extensions/Dev.py
@@ -1,11 +1,11 @@
-import typing
+from typing import Optional, Union
 from json import dump
 from io import StringIO
 
 import discord
 from discord.ext import commands, menus
 
-from utils import checks
+from utils import checks, bots
 
 
 class ShardNotFound(Exception):
@@ -51,7 +51,7 @@ class ShardMenu(menus.Menu):
     async def send_initial_message(self, ctx, channel):
         embed = (
             discord.Embed(
-                title=f"Info for shard {self.shard_info.id}/{self.shard_info.shard_count}",
+                title=f"Info for shard {self.shard_info.id + 1}/{self.shard_info.shard_count}",
             )
             .add_field(name="Online:", value=str(not self.shard_info.is_closed()))
             .add_field(name="Latency:", value=f"{round(self.shard_info.latency * 1000)} ms")
@@ -66,10 +66,10 @@ class ShardMenu(menus.Menu):
 class Dev(commands.Cog):
     """Tools to be used by the bots developer to operate the bots."""
 
-    def __init__(self, bot):
+    def __init__(self, bot: bots.BOT_TYPES) -> None:
         self.bot = bot
 
-    async def cog_check(self, ctx):
+    async def cog_check(self, ctx: bots.CustomContext) -> bool:
         if not await ctx.bot.is_owner(ctx.author):
             raise commands.NotOwner('You do not own this bot.')
         return True
@@ -81,23 +81,47 @@ class Dev(commands.Cog):
         description="Change the bots's nickname, for situations where you do not have privileges to.",
     )
     @commands.guild_only()
-    async def nick(self, ctx, *, name: typing.Optional[str]):
+    async def nick(self, ctx: bots.CustomContext, *, name: Optional[str]) -> None:
         await ctx.guild.me.edit(nick=name)
 
     @commands.command(
-        name="raw",
-        aliases=["document"],
-        brief="Get raw document for an entity.",
-        description="Prints an entity's raw document. Be careful! It can contain sensitive information.",
+        name="guildraw",
+        aliases=["document", "raw", "guilddocument"],
+        brief="Get raw document for a guild.",
+        description="Prints an guild's raw document. Be careful! It can contain sensitive information.",
     )
-    async def raw(self, ctx, entity: typing.Optional[typing.Union[discord.Guild, discord.Member, discord.User]]):
+    async def guildraw(
+            self,
+            ctx:
+            bots.CustomContext, entity: Optional[Union[discord.Guild, discord.Member, discord.User]],
+    ) -> None:
         entity = entity or ctx.guild
-        if isinstance(entity, discord.Guild):
-            document = await ctx.bot.get_guild_document(entity)
-        elif isinstance(entity, (discord.Member, discord.User)):
-            document = await ctx.bot.get_user_document(entity)
+        document = await ctx.bot.get_guild_document(entity)
+
         buffer = StringIO()
         dump(document, buffer)
+
+        buffer.seek(0)
+        file = discord.File(buffer, f"{entity.id}.json")
+        await ctx.send(file=file)
+
+    @commands.command(
+        name="userraw",
+        aliases=["userdocument"],
+        brief="Get raw document for a user.",
+        description="Prints an user's raw document. Be careful! It can contain sensitive information.",
+    )
+    async def userraw(
+            self,
+            ctx: bots.CustomContext,
+            entity: Optional[Union[discord.Guild, discord.Member, discord.User]],
+    ) -> None:
+        entity = entity or ctx.author
+        document = await ctx.bot.get_user_document(entity)
+
+        buffer = StringIO()
+        dump(document, buffer)
+
         buffer.seek(0)
         file = discord.File(buffer, f"{entity.id}.json")
         await ctx.send(file=file)
@@ -109,7 +133,7 @@ class Dev(commands.Cog):
         description="Gets info on a shard and presents a menu which can be used to manage the shard.",
     )
     @commands.check(checks.bot_is_sharded)
-    async def shard_info(self, ctx, *, shard_id: typing.Optional[typing.Union[discord.Guild, int]]):
+    async def shard_info(self, ctx: bots.CustomContext, *, shard_id: Optional[Union[discord.Guild, int]]) -> None:
         shard_id = shard_id or ctx.guild
 
         if isinstance(shard_id, discord.Guild):
@@ -127,9 +151,9 @@ class Dev(commands.Cog):
         brief="Lists all guilds the bot is in.",
         description="Lists all guilds that the bot is in. May contain sensitive information!"
     )
-    async def guilds(self, ctx):
+    async def guilds(self, ctx: bots.CustomContext) -> None:
         await menus.MenuPages(GuildsMenuList(ctx.bot.guilds, per_page=10)).start(ctx)
 
 
-def setup(bot):
+def setup(bot: bots.BOT_TYPES):
     bot.add_cog(Dev(bot))

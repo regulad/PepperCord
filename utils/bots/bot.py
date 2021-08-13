3dfe872ee06105ec299d80ab71592afb5784f1ef
regulad <52430642+regulad@users.noreply.github.com>
2021-08-13 09:29:32 -0400

    Experimental context_creation event

--- utils/bots/bot.py
+++ utils/bots/bot.py
@@ -1,4 +1,5 @@
-from typing import Union, List, Any
+import logging
+from typing import Union, List, Any, Type, cast
 
 import discord
 from discord.ext import commands
@@ -76,18 +77,69 @@ class CustomBotBase(commands.bot.BotBase):
 
         return await Document.get_document(self._database["user"], {"_id": model.id})
 
-    async def get_context(self, message, *, cls=CustomContext):
-        result = await super().get_context(message, cls=cls)
-        if isinstance(result, CustomContext):
-            await result.get_documents()
+    async def get_context(self, message, *, cls: Type[commands.Context] = CustomContext):
+        result: cls = await super().get_context(message, cls=cls)
+        await self.wait_for_dispatch("context_creation", result)
         return result
 
+    async def on_context_creation(self, ctx: commands.Context) -> None:  # Placeholder method
+        if isinstance(ctx, CustomContext):
+            await cast(CustomContext, ctx).get_documents()
 
-class CustomAutoShardedBot(CustomBotBase, discord.AutoShardedClient):
+    async def wait_for_dispatch(self, event_name, *args, **kwargs):
+        await super().wait_for_dispatch(event_name, *args, **kwargs)
+        ev = 'on_' + event_name
+        for event in self.extra_events.get(ev, []):
+            await super()._schedule_event(event, ev, *args, **kwargs)
+
+
+class CustomClientBase:
+    async def wait_for_dispatch(self, event: str, *args, **kwargs):
+        logging.debug('Dispatching event %s', event)
+        method = 'on_' + event
+
+        listeners = self._listeners.get(event)
+        if listeners:
+            removed = []
+            for i, (future, condition) in enumerate(listeners):
+                if future.cancelled():
+                    removed.append(i)
+                    continue
+
+                try:
+                    result = condition(*args)
+                except Exception as exc:
+                    future.set_exception(exc)
+                    removed.append(i)
+                else:
+                    if result:
+                        if len(args) == 0:
+                            future.set_result(None)
+                        elif len(args) == 1:
+                            future.set_result(args[0])
+                        else:
+                            future.set_result(args)
+                        removed.append(i)
+
+            if len(removed) == len(listeners):
+                self._listeners.pop(event)
+            else:
+                for idx in reversed(removed):
+                    del listeners[idx]
+
+        try:
+            coro = getattr(self, method)
+        except AttributeError:
+            pass
+        else:
+            await self._schedule_event(coro, method, *args, **kwargs)
+
+
+class CustomAutoShardedBot(CustomBotBase, CustomClientBase, discord.AutoShardedClient):
     pass
 
 
-class CustomBot(CustomBotBase, discord.Client):
+class CustomBot(CustomBotBase, CustomClientBase, discord.Client):
     pass
 
 

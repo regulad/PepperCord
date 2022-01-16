5a64339e805c2c25f205121935dc1f2a624d5b62
Parker Wahle <parkeredwardwahle2017@gmail.com>
2022-01-16 14:48:16 -0500

    belated webhook support

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ https://github.com/regulad/PepperCord
 
 import logging
 import os
-from typing import Optional, List, Type
+from typing import Optional, List, Type, MutableMapping
 
 import art
 import discord
+from dislog import DiscordWebhookHandler
 from dotenv import load_dotenv
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
 from pretty_help import PrettyHelp
@@ -19,27 +20,36 @@ if __name__ == "__main__":
     if os.path.exists(".env"):
         load_dotenv()
 
-    if not os.path.exists("config/"):
-        os.mkdir("config/")
+    config_source: MutableMapping = os.environ
+
+    debug: bool = config_source.get("PEPPERCORD_DEBUG") is not None
 
     logging.basicConfig(
-        level=logging.INFO, format="%(asctime)s:%(levelname)s:%(name)s: %(message)s"
+        level=logging.DEBUG if debug else logging.INFO, format="%(asctime)s:%(levelname)s:%(name)s: %(message)s"
     )
+
+    maybe_webhook: Optional[str] = config_source.get("PEPPERCORD_WEBHOOK")
+
+    if maybe_webhook is not None:
+        logging.root.addHandler(DiscordWebhookHandler(maybe_webhook))
+
+    if not os.path.exists("config/"):
+        os.mkdir("config/")
 
     logging.info("Configuring database connection...")
     # Configure the database
     db_client: AsyncIOMotorClient = AsyncIOMotorClient(
-        os.environ.get("PEPPERCORD_URI", "mongodb://mongo")
+        config_source.get("PEPPERCORD_URI", "mongodb://mongo")
     )
     db: AsyncIOMotorDatabase = db_client[
-        os.environ.get("PEPPERCORD_DB_NAME", "peppercord")
+        config_source.get("PEPPERCORD_DB_NAME", "peppercord")
     ]
     logging.info("Done.")
 
     logging.info("Configuring bot...")
     # Configure Sharding
     bot_class: Type[bots.BOT_TYPES]
-    shards: Optional[int] = int(os.environ.get("PEPPERCORD_SHARDS", "0"))
+    shards: Optional[int] = int(config_source.get("PEPPERCORD_SHARDS", "0"))
     if shards > 0:
         bot_class = bots.CustomAutoShardedBot
     elif shards == -1:
@@ -50,7 +60,7 @@ if __name__ == "__main__":
         shards = None
     # Configure bot
     bot: bots.BOT_TYPES = bot_class(
-        command_prefix=os.environ.get("PEPPERCORD_PREFIX", "?"),
+        command_prefix=config_source.get("PEPPERCORD_PREFIX", "?"),
         case_insensitive=True,
         help_command=PrettyHelp(
             color=discord.Colour.orange(),
@@ -62,13 +72,13 @@ if __name__ == "__main__":
         config=os.environ,
         shard_count=shards,
         slash_commands=True,
-        message_commands=bool(os.environ.get("PEPPERCORD_DEBUG")),
+        message_commands=debug,
         slash_command_guilds=(
             [
                 int(testguild)
-                for testguild in os.environ["PEPPERCORD_TESTGUILDS"].split(", ")
+                for testguild in config_source["PEPPERCORD_TESTGUILDS"].split(", ")
             ]
-            if os.environ.get("PEPPERCORD_TESTGUILDS") is not None
+            if config_source.get("PEPPERCORD_TESTGUILDS") is not None
             else None
         ),
     )
@@ -85,10 +95,12 @@ if __name__ == "__main__":
             if file.endswith(".py"):
                 full_path: str = f"{directory}/" + file
                 bot.load_extension(os.path.splitext(full_path)[0].replace("/", "."))
-    bot.load_extension("jishaku")  # Special case.
+    if debug:
+        bot.load_extension("jishaku")
+
     logging.info("Done.")
 
     logging.info("Ready.")
     logging.info(f"\n{art.text2art('PepperCord', font='rnd-large')}")
 
-    bot.run(os.environ.get("PEPPERCORD_TOKEN"))
+    bot.run(config_source.get("PEPPERCORD_TOKEN"))

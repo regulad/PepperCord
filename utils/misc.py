a6574e30ab3ca75aa5ea791887fb4da356792fd3
regulad <regulad@outlook.com>
2022-01-20 11:06:22 -0500

    Done with abstract?

--- utils/misc.py
+++ utils/misc.py
@@ -1,2 +1,39 @@
+import collections
+
+
 def split_string_chunks(string: str, chunk_size: int = 2000) -> list[str]:
     return [string[i:i + chunk_size] for i in range(0, len(string), chunk_size)]
+
+
+class FrozenDict(collections.Mapping):
+    def __init__(self, *args, **kwargs):
+        self._d = dict(*args, **kwargs)
+        self._hash = None
+
+    def __iter__(self):
+        return iter(self._d)
+
+    def __len__(self):
+        return len(self._d)
+
+    def __getitem__(self, key):
+        return self._d[key]
+
+    def __hash__(self):
+        # It would have been simpler and maybe more obvious to
+        # use hash(tuple(sorted(self._d.iteritems()))) from this discussion
+        # so far, but this solution is O(n). I don't know what kind of
+        # n we are going to run into, but sometimes it's hard to resist the
+        # urge to optimize when it will gain improved algorithmic performance.
+        if self._hash is None:
+            hash_ = 0
+            for pair in self.items():
+                hash_ ^= hash(pair)
+            self._hash = hash_
+        return self._hash
+
+
+__all__: list[str] = [
+    "split_string_chunks",
+    "FrozenDict"
+]

3c72c5cfba967eb97152fbb56646e3ef1b6b273e
Parker Wahle <parkeredwardwahle2017@gmail.com>
2022-05-12 19:50:36 -0400

    Fix message commands

--- utils/bots/context.py
+++ utils/bots/context.py
@@ -24,8 +24,6 @@ class _DefaultSendHandler(SendHandler):
         if self.ctx.interaction is None:
             if kwargs.get("ephemeral") is not None:
                 del kwargs["ephemeral"]
-            if kwargs.get("return_message") is not None:
-                del kwargs["return_message"]
             if kwargs.get("reference") is None:
                 kwargs["reference"] = self.ctx.message
             return await self.ctx.send_bare(*args, **kwargs)
@@ -38,8 +36,6 @@ class _DefaultSendHandler(SendHandler):
                 except NotFound:
                     if kwargs.get("ephemeral") is not None:
                         del kwargs["ephemeral"]
-                    if kwargs.get("return_message") is not None:
-                        del kwargs["return_message"]
                     return await self.ctx.channel.send(*args, **kwargs)  # Worst case
 
 
@@ -91,12 +87,10 @@ class CustomContext(commands.Context):
             try:
                 await super().defer(ephemeral=ephemeral)
             except discord.NotFound:
-                if self.interaction is not None:
-                    await super().channel.trigger_typing()
-                else:
-                    raise
-        else:
-            await super().channel.trigger_typing()
+                if trigger_typing:
+                    await super().channel.typing()
+        elif trigger_typing:
+            await super().channel.typing()
 
     def send(self, *args, **kwargs) -> Coroutine[Any, Any, Message]:
         return self.send_handler.send(*args, **kwargs)
